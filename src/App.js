791b358c125c61af8f7db7303a7979768629dcb4
Thermiee <navyblack16@yahoo.com>
2023-06-26 22:09:24 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Contact from './pages/Contact';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Home</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" caseSensitive={false} element={<Home />} />
+        <Route path="/dashboard" caseSensitive={false} element={<Dashboard />} />
+        <Route path="/contact" caseSensitive={false} element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
